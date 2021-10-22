a6e0d17a7ba9ba44f5b75f0800a7ea4782def732
Andre Flores <oaflores7@utpl.edu.ec>
2021-10-22 11:27:30 -0500

    primer commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -16,8 +16,9 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-  System.out.println("Ejemplo básico de Java");
+  System.out.println("Ejemplo de Java");
+  System.out.println("Manejado desde GitHub");
   System.out.println("Versionado a través de Git");
-  System.out.println("Manejado desde GitHub");  }
+  System.out.println("Creado por andref45");}
     
 }
